fed523c971218895d7e4901d3c3bf1bba2dd2bda
yanggang 양갱이 <tnqlsrhksk00@gmail.com>
2023-11-16 03:45:40 +0000

    hw2

--- application.py
+++ application.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 from database import DBhandler
+import hashlib
 import sys
 application = Flask(__name__)
+application.config["SECRET_KEY"] = "helloosp"
 
 DB = DBhandler()
 
@@ -9,6 +11,25 @@ DB = DBhandler()
 @application.route("/")
 def hello():
     return render_template("index.html")
+
+
+@application.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@application.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+@application.route("/signup_post", methods = ['POST'])
+def register_user():
+    data = request.form
+    pw = request.form['pw']
+    pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
+    if DB.insert_user(data, pw_hash):
+        return render_template("login.html")
 
 
 @application.route("/list")
